ead7d17ed29aef9b62daecba90a1dcf1a64fbca1
Raymond <rtperez1020@gmail.com>
2024-02-18 15:08:01 -0800

    update

--- script.js
+++ script.js
@@ -6,23 +6,21 @@ const expenseInput = document.querySelector('#input-expenses')
 const incomeDisplay = document.querySelector('[data-income-display')
 const expenseList = document.querySelector('[data-expense-list]')
 const expenseTotalDisplay = document.querySelector('[data-expense-total')
-const remainingBalanceDisplay = document.querySelector('#remaining-balance-display')
+const remainingBalanceDisplay = document.querySelector('[data-remaining-balance-display]')
+
 
 let expenseTotal = 0
 let incomeTotal = 0
 let remainingBalance = 0
 
+updateDisplay()
+
 incomeForm.addEventListener('submit', e => {
     e.preventDefault()
-    if(incomeInput.value == ''){
-        incomeDisplay.innerText = 'Income: $0.00'
-    }else{
-        incomeDisplay.innerText = 'Income: $'+incomeInput.value
-        incomeTotal = parseInt(incomeInput.value)
-        remainingBalance = incomeTotal - expenseTotal
-        remainingBalanceDisplay.innerText = 'Remaining Balance: $' + remainingBalance
-        incomeInput.value = ''
-    }
+
+    incomeTotal = parseInt(incomeInput.value)
+    updateDisplay()
+    incomeInput.value = ''
 })
 
 
@@ -30,28 +28,30 @@ expenseForm.addEventListener('submit', e => {
     e.preventDefault()
 
     const expenseItem = document.createElement('div')
-    expenseItem.innerText=expenseName.value + " " + expenseInput.value
-    expenseItem.classList.add('expense')
+    expenseItem.innerText=`${expenseName.value}: $${expenseInput.value}`
+    expenseItem.expense = parseInt(expenseInput.value)
     expenseList.append(expenseItem)
     
-    if(expenseInput.value != ''){
-        expenseTotal += parseInt(expenseInput.value)
-        expenseTotalDisplay.innerText = 'Total Expenses: $' + expenseTotal
-        console.log(expenseTotal)
-    }
+    expenseTotal += expenseItem.expense
+    updateDisplay()
+    
     expenseInput.value = ''
     expenseName.value = ''
     
-
     expenseItem.addEventListener("click", ()=>{
-        
-        expenseTotal -= parseInt(expenseItem.value)
-        remainingBalance = incomeTotal-expenseTotal
-        console.log(expenseTotal)
-
-        remainingBalanceDisplay.innerText = 'Remaining Balance: $' + remainingBalance
-        expenseTotalDisplay.innerText = 'Total Expenses: $' + expenseTotal
+        expenseTotal -= expenseItem.expense
+        updateDisplay()
         expenseItem.remove()
     })
+    
 })
 
+function updateDisplay(){
+    if(expenseTotal < 0 ){
+        expenseTotal = 0
+    }
+    remainingBalance = incomeTotal - expenseTotal
+    incomeDisplay.innerText = 'Income: $'+incomeTotal
+    remainingBalanceDisplay.innerText = `Remaining Balance: $${remainingBalance}`
+    expenseTotalDisplay.innerText = `Total Expenses: $ ${expenseTotal}`  
+}
